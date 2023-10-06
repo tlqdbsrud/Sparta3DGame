d9e4cea0165b4f91bae8b483808987a02f7494b5
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-06 18:33:05 +0900

    [KYG_ADD]PlayerController 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody _rigidbody;
+
+    // 싱글톤
+    public static PlayerController instance; 
+
+    private void Awake()
     {
-        
+        instance = this;
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; // 1인칭이면 커서가 있으면 안되므로 잠굼
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+   
+    private void LateUpdate()
+    {
+        if (canLook)
+        {
+            CameraLook();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x; // 서 있는 상태
+        dir *= moveSpeed; // 움직임 속도
+        dir.y = _rigidbody.velocity.y; // 
+
+        _rigidbody.velocity = dir;
+    }
+
+    // 카메라 마우스 이동
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity; // x축이 
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    // 바라보기
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    // 이동
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed) // 움직일 때
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled) // 멈출 때
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    // 점프
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started) // 한 번 누를 때 점프 한 번
+        {
+            if (IsGrounded())
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (Vector3.up * 0.01f) , Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f)+ (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
+    }
+
+    public void ToggleCursor(bool toggle)
+    {
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+        canLook = !toggle;
     }
 }
